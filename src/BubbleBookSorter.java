862b42f8d07b9d9840cd4888024e9c8465e855f0
sony <sony@sony-laptope.(none)>
2012-12-27 15:23:28 +0000

    Première modif by MAIGA

--- src/BubbleBookSorter.java
+++ src/BubbleBookSorter.java
@@ -6,7 +6,7 @@ public class BubbleBookSorter implements IBookSorter{
 	public ArrayList<Book> sortBooks(ArrayList<Book> unsortedBooks){
 		ArrayList<Book> sorted = new ArrayList<Book>();
 		// implement the code here						
-		System.out.println("Books sorted using Bubble algorithm...");
+		System.out.println("Books sorted using Bubble algorithm..." + "implementer par MAIGA");
 		return unsortedBooks;					
 	}
 	
